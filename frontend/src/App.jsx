296f511e94eef0eda7324973166a405909431beb
Suman Kumar <sumanqaj9876@gmail.com>
2026-04-13 00:50:52 +0530

    added floating numbers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 
-import React from 'react';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import LandingPage from './pages/LandingPage';
+import FloatingButtons from './components/common/FloatingPoint';
 
 const App = () => {
   return (
     <div className='font-canela min-h-screen flex flex-col'>
+           <FloatingButtons />
       <Navbar />
       <div className='flex-1'>
         <LandingPage/>
